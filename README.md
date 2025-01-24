d1b6e877372d52e0cf140be108d08db819b4447d
josh warren <joshnwarren@gmail.com>
2025-01-23 21:15:00 -0800

    dev: update start-script & readme

--- README.md
+++ README.md
@@ -6,12 +6,21 @@
 npm run dev
 ```
 
-if the permissions are not correct, run the following command:
+**Possible Errors**
+
+1. if the permissions are not correct, run the following command:
 
 ```sh
 chmod +x start-script.sh
 ```
 
-then try again.
+2. if you get an error like this: `permission denied while trying to connect to the Docker daemon socket at ...`
+
+run the following command:
+
+```
+sudo usermod -aG docker $USER
+newgrp docker
+```
 
 3. Open your browser and navigate to http://localhost:3000
--- start-script.sh
+++ start-script.sh
@@ -1,14 +1,24 @@
 #!/bin/bash
 
-yarn docker:up
+cleanup() {
+  echo "Stopping the Docker container..."
+  npm run docker:down
+  kill $frontend_pid $backend_pid 2>/dev/null
+  exit
+}
 
-npm install && cd frontend && npm install --legacy-peer-deps && wait-on tcp:3306 && yarn dev & 
+# Trap SIGINT (Ctrl+C) and call the cleanup function
+trap cleanup SIGINT
+
+npm run docker:up
+
+npm install && cd frontend && npm install --legacy-peer-deps && wait-on tcp:3306 && npm run dev & 
 frontend_pid=$!
 
-npm install && cd backend && npm install --legacy-peer-deps && wait-on tcp:3306 && yarn dev & 
+npm install && cd backend && npm install --legacy-peer-deps && wait-on tcp:3306 && npm run dev & 
 backend_pid=$!
 
 wait $frontend_pid
 wait $backend_pid
 
-yarn docker:down
+cleanup
