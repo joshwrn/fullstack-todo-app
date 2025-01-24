9c9a0a3f57af372bd54d8e1712d2b49bbba2ae37
josh warren <joshnwarren@gmail.com>
2025-01-23 21:03:27 -0800

    cleanup: delete node modules

--- backend/index.ts
+++ backend/index.ts
@@ -1,13 +1,71 @@
 import express from 'express'
+import cors from 'cors'
+import { Prisma, PrismaClient, Task } from '@prisma/client'
+
+const prisma = new PrismaClient()
 
 const port = process.env.PORT || 8080
 
 const app = express()
+app.use(cors({ origin: 'http://localhost:3000' }), express.json())
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+app.get('/tasks', async (req, res) => {
+  console.log('getting tasks')
+  const tasks = await prisma.task.findMany()
+  res.json(tasks)
+})
+
+app.post('/tasks', async (req, res) => {
+  const task = req.body as Pick<Task, 'title' | 'color'>
+  if (!task.title) {
+    console.log('title is required')
+    res.status(400).json({ message: 'Title is required' })
+    return
+  }
+  if (!task.color) {
+    console.log('color is required')
+    res.status(400).json({ message: 'Color is required' })
+    return
+  }
+  const newTask = await prisma.task.create({
+    data: {
+      description: '',
+      completed: false,
+      createdAt: new Date(),
+      ...task,
+    },
+  })
+  res.json(newTask)
+  return
+})
+
+app.patch('/tasks/:id', async (req, res) => {
+  const { id } = req.params
+  const { update } = req.body as { update: Prisma.TaskUpdateInput }
+  if (!update.title) {
+    res.status(400).json({ message: 'Title is required' })
+    return
+  }
+  if (!update.color) {
+    console.log('color is required')
+    res.status(400).json({ message: 'Color is required' })
+    return
+  }
+  const updatedTask = await prisma.task.update({
+    where: { id },
+    data: update,
+  })
+  res.json(updatedTask)
+})
+
+app.delete('/tasks/:id', async (req, res) => {
+  const { id } = req.params
+  const deletedTask = await prisma.task.delete({
+    where: { id },
+  })
+  res.json(deletedTask)
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`Tasks API listening on port ${port}`)
 })
--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,3 +1,127 @@
+'use client'
+import { useMutation, useQuery } from 'react-query'
+import Link from 'next/link'
+import { BASE_URL } from '@/constants/base-url'
+import { Task } from '@/schemas/task-schema'
+import { GoTrash as TrashIcon } from 'react-icons/go'
+import { useSetTaskData } from '../hooks/use-set-task-data'
+import { useQueryParams } from '../hooks/use-query-params'
+import { EditTask } from './edit-task'
+import { useSearchParams } from 'next/navigation'
+import clsx from 'clsx'
+import { returnColor } from '@/components/colors'
+import { QUERY_KEYS } from '@/constants/query-keys'
+
 export default function Home() {
-  return null
+  const setTaskData = useSetTaskData()
+  const queryParams = useQueryParams()
+  const searchParams = useSearchParams()
+  const query = useQuery<Task[]>(
+    QUERY_KEYS.tasks,
+    async () => {
+      const response = await fetch(`${BASE_URL}/tasks`)
+      return await response.json()
+    },
+    {
+      staleTime: Infinity,
+    }
+  )
+
+  const completeTaskMutation = useMutation(
+    ['completeTask'],
+    async (input: { id: string; newValue: boolean }) => {
+      setTaskData.update(input.id, { completed: input.newValue })
+      try {
+        await fetch(`${BASE_URL}/tasks/${input.id}`, {
+          method: 'PATCH',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({ update: { completed: input.newValue } }),
+        })
+      } catch (error) {
+        setTaskData.update(input.id, { completed: !input.newValue })
+        console.error(error)
+      }
+    }
+  )
+
+  const deleteTaskMutation = useMutation(
+    ['deleteTask'],
+    async (input: { id: string }) => {
+      const taskToDelete = query.data?.find((task) => task.id === input.id)
+      const taskIndex = query.data?.findIndex((task) => task.id === input.id)
+      setTaskData.remove(input.id)
+      try {
+        await fetch(`${BASE_URL}/tasks/${input.id}`, {
+          method: 'DELETE',
+        })
+        setTaskData.remove(input.id)
+      } catch (error) {
+        setTaskData.add(taskToDelete, taskIndex)
+        console.error(error)
+      }
+    }
+  )
+
+  return (
+    <div className="flex flex-col items-center justify-start h-screen w-screen bg-slate-950 p-10">
+      <main className="flex flex-col items-center justify-center w-full p-4 bg-slate-900 max-w-xl rounded-xl">
+        <header className="flex w-full items-center relative px-4 justify-between ">
+          <h1>Todo List</h1>
+          <Link
+            href="/create-task"
+            className="border border-slate-700 p-2 rounded-lg bg-slate-800"
+          >
+            <button>
+              <p className="text-slate-300">Create Task</p>
+            </button>
+          </Link>
+        </header>
+        <section className="flex w-full flex-col items-center justify-center gap-4 p-4 text-slate-300">
+          {searchParams.get('id') && <EditTask />}
+          {query.data?.map((task) => (
+            <div
+              className="flex w-full border border-slate-700 p-2 rounded-lg bg-slate-800 cursor-pointer gap-2 items-center"
+              key={task.id}
+              onClick={() => {
+                queryParams.push('id', task.id)
+              }}
+            >
+              <input
+                type="checkbox"
+                checked={task.completed}
+                onClick={(e) => {
+                  e.stopPropagation()
+                }}
+                onChange={() => {
+                  completeTaskMutation.mutate({
+                    id: task.id,
+                    newValue: !task.completed,
+                  })
+                }}
+                className="cursor-pointer"
+              />
+              <div
+                className={clsx(
+                  returnColor(task.color),
+                  `w-2 h-2 rounded-full ml-2`
+                )}
+              />
+              {task.title}
+              <button
+                className="ml-auto"
+                onClick={(e) => {
+                  e.stopPropagation()
+                  deleteTaskMutation.mutate({ id: task.id })
+                }}
+              >
+                <TrashIcon />
+              </button>
+            </div>
+          ))}
+        </section>
+      </main>
+    </div>
+  )
 }
